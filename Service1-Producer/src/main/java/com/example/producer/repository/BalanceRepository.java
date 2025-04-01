0b018ed680159ffea0a14d4a7abf135d364e69a1
Nikita222555 <nikita0404975@mail.ru>
2025-04-01 13:56:58 +0300

    add new service

---
+++ Service1-Producer/src/main/java/com/example/producer/repository/BalanceRepository.java
@@ -0,0 +1,24 @@
+/**
+ * BalanceRepo
+ *
+ * @author Tomilov
+ * @version 1.0
+ */
+
+package com.example.producer.repository;
+
+import com.example.producer.model.Balance;
+import com.example.producer.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface BalanceRepository extends JpaRepository<Balance, Long> {
+
+    Optional<Balance> findByClient(Client client);
+
+}
+
+
